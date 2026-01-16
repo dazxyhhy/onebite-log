21b419723ea6cec75d4639ce57e7ee0a698df982
양다혜 <yangdahye.e@daum.net>
2026-01-16 18:15:48 +0900

    feat: 글로벌 레이아웃 설정하기

--- src/root-route.tsx
+++ src/root-route.tsx
@@ -1,25 +1,28 @@
 import { Navigate, Route, Routes } from "react-router";
-import SignInPage from "./pages/Sign-in-page";
+import SignInPage from "./pages/sign-in-page";
 import SignOutPage from "./pages/sign-up-page";
 import ForgetPasswordPage from "./pages/forget-password-page";
 import PostDetailPage from "./pages/post-detail-page";
 import ProfileDetailPage from "./pages/profile-detail-page";
 import ResetPasswordPage from "./pages/reset-password-page";
 import IndexPage from "./pages/index-page";
+import GlobalLayout from "./components/ui/layout/global-layout";
 
 export default function RootRoute() {
   return (
     <Routes>
-      <Route path = "/sign-in" element={<SignInPage/>}/>
-      <Route path = "/sign-up" element={<SignOutPage/>}/>
-      <Route path = "/forget-password" element={<ForgetPasswordPage/>} />
+      <Route element={<GlobalLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignOutPage />} />
+        <Route path="/forget-password" element={<ForgetPasswordPage />} />
 
-      <Route path = "/" element={<IndexPage/>}/>
-      <Route path = "/post/:postId" element={<PostDetailPage/>}/>
-      <Route path = "/profile/:userId" element={<ProfileDetailPage/>}/>
-      <Route path = "/reset-password" element={<ResetPasswordPage/>}/>
-      
-      <Route path = "*" element = {<Navigate to = {'/'} />}/>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/post/:postId" element={<PostDetailPage />} />
+        <Route path="/profile/:userId" element={<ProfileDetailPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Route>
     </Routes>
   );
 }
